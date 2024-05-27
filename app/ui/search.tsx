e54bafcc327e13ff7933603b174adf481e9eb458
Isaac Olguin <misael.olguin@gmail.com>
2024-05-28 01:30:14 +0200

    Update of project

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,47 @@
-'use client';
+'use client'; // This is a Client Component which means you can use event listeners and hooks
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+//export default function Search() {
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  /*function handleSearch(term: string) {
+    //console.log(term)
+    console.log(`Searching... ${term}`)
+
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+    // pathname is the current path
+    // params.toString(): as the user types into the search bar, it translates this input into a URL-friendly format
+    // replace(): update the URL with the user's search data. For example "/abc/def?query=XXX" if the user searches for XXX
+    // ==> This URL is updated without reloading the page, thanks to Next.js's client-side navigation
+  }*/
+  // Inside the Search Component...
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+  
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,7 +50,15 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value)
+        }}
+        defaultValue={searchParams.get("query")?.toString()}
       />
+      {/*
+        defaultValue vs. value / Controlled vs. Uncontrolled
+      */}
+
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
